8c5e5c3ea849e98266809c5f34878c06831cd86c
Simon LOIR <simon.loir@student.unamur.be>
2021-09-02 22:20:14 +0200

    added a way to retreive a list of customers

--- src/customers.ts
+++ src/customers.ts
@@ -2,4 +2,11 @@ import EZBizSDK from '.';
 
 export default class EZCustomerAPI {
     constructor(private sdk: EZBizSDK) {}
+
+    /**
+     * @returns The list of customers of the enterprise
+     */
+    public list() {
+        return this.sdk.GET('customers');
+    }
 }
